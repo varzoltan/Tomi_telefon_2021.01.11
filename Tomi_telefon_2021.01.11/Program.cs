b5dac56e099a4c854ae6c1929f63243f65a2bd0b
varzoltan <64203391+varzoltan@users.noreply.github.com>
2021-01-11 15:41:04 +0100

    Add project files.

---
+++ Tomi_telefon_2021.01.11/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tomi_telefon_2021._01._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
