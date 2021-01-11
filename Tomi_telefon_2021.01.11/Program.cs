a718fd747a1a7e0877bb7145459e6ac106c9fc31
varzoltan <64203391+varzoltan@users.noreply.github.com>
2021-01-11 15:49:57 +0100

    15:49

--- Tomi_telefon_2021.01.11/Program.cs
+++ Tomi_telefon_2021.01.11/Program.cs
@@ -8,8 +8,23 @@ namespace Tomi_telefon_2021._01._11
 {
     class Program
     {
+        struct Adat
+        {
+            public int beerkezoora;
+            public int beerkezoperc;
+            public int beerkezomasodperc;
+            public int kimenoora;
+            public int kimenoperc;
+            public int kimenomasodperc;
+        }
         static void Main(string[] args)
         {
+
+        }
+        static int mbe(int o,int p,int mp)
+        {
+            int masodperc = (o * 3600) + (p * 60) + mp;
+            return masodperc;
         }
     }
 }
